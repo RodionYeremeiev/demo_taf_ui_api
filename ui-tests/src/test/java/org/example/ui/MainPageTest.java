be11595b59296e9bb54ddc4219a81087a84f7392
Rod <62612622+RodionYeremeiev@users.noreply.github.com>
2025-11-03 18:03:51 +0200

    clean-up (#2)
    * remove and ignore merged report dirs
    
    * minor refactor
    
    ---------
    
    Co-authored-by: Rod <ryitlearning@gmail.com>

--- ui-tests/src/test/java/org/example/ui/MainPageTest.java
+++ ui-tests/src/test/java/org/example/ui/MainPageTest.java
@@ -1,4 +1,5 @@
 package org.example.ui;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -11,40 +12,43 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
-    MainPage mainPage = new MainPage();
+  MainPage mainPage = new MainPage();
 
-@BeforeAll    public static void setUpAll() {
-        Configuration.browserSize = "1280x800";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
+  @BeforeAll
+  public static void setUpAll() {
+    Configuration.browserSize = "1280x800";
+    SelenideLogger.addListener("allure", new AllureSelenide());
+  }
 
-@BeforeEach    public void setUp() {
-        open("https://www.jetbrains.com/");
-    }
+  @BeforeEach
+  public void setUp() {
+    open("https://www.jetbrains.com/");
+  }
 
-    @Test
-    public void search() {
-        mainPage.searchButton.click();
+  @Test
+  public void search() {
+    mainPage.searchButton.click();
 
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
+    $("[data-test='search-input']").sendKeys("Selenium");
+    $("button[data-test='full-search-button']").click();
 
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
-    }
+    $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+  }
 
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
+  @Test
+  public void toolsMenu() {
+    mainPage.toolsMenu.click();
 
-        $("div[data-test='main-submenu']").shouldBe(visible);
-    }
+    $("div[data-test='main-submenu']").shouldBe(visible);
+  }
 
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
+  @Test
+  public void navigationToAllTools() {
+    mainPage.seeDeveloperToolsButton.click();
+      mainPage.findYourToolsButton.click();
 
-        $("#products-page").shouldBe(visible);
+    $("#products-page").shouldBe(visible);
 
-assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());    }
+    assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+  }
 }
