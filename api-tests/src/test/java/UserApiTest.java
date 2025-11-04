f2441b99e0ecb0c7ac197b9006ea1035497c9689
Rod <ryitlearning@gmail.com>
2025-11-04 16:44:46 +0200

    add initial tests

--- api-tests/src/test/java/UserApiTest.java
+++ api-tests/src/test/java/UserApiTest.java
@@ -1,18 +1,51 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class UserApiTest {
+
+  @BeforeAll
+  static void setup() {
+    RestAssured.baseURI = "https://reqres.in/api";
+  }
+
   @Test
-  void getUserShouldReturn200() {
-    RestAssured.baseURI = "https://api.example.com";
+  void getListOfUsers() {
     given()
             .when()
-            .get("/users/1")
+            .get("/users?page=2")
             .then()
             .statusCode(200)
-            .body("name", equalTo("John Doe"));
+            .body("page", equalTo(2))
+            .body("data", not(empty()));
   }
+
+  @Test
+  void getSingleUser() {
+    given()
+            .when()
+            .get("/users/2")
+            .then()
+            .statusCode(200)
+            .body("data.id", equalTo(2));
+  }
+
+  @Test
+  void createUser() {
+    given()
+            .contentType(ContentType.JSON)
+            .body("{\"name\": \"Rod\", \"job\": \"QA\"}")
+            .when()
+            .post("/users")
+            .then()
+            .statusCode(201)
+            .body("name", equalTo("Rod"))
+            .body("job", equalTo("QA"))
+            .body("id", notNullValue());
+  }
+
 }
