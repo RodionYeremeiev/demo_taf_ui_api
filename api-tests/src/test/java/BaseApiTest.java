c56adb2302e5e738a2de7e39e2fd65a23759a62d
Rod <ryitlearning@gmail.com>
2025-11-04 20:55:29 +0200

    update test (models and assertions)

--- api-tests/src/test/java/BaseApiTest.java
+++ api-tests/src/test/java/BaseApiTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.BeforeAll;
 
 public abstract class BaseApiTest {
 
+    public static final String ERROR_NOT_AS_EXPECTED = "Error: %s is not as expected";
+    public static final String ERROR_MUST_NOT_BE_EMPTY = "Error: %s must no be empty";
+    public static final String ERROR_INVALID_API_KEY = "Invalid or inactive API key";
+
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://reqres.in/api";
--- api-tests/src/test/java/UserApiTest.java
+++ api-tests/src/test/java/UserApiTest.java
@@ -1,71 +1,98 @@
-
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import io.qameta.allure.Description;
 import io.restassured.http.ContentType;
+import model.User;
+import model.UserResponse;
+import model.getsingleormultiuserresponse.GetMultiUserResponse;
+import model.getsingleormultiuserresponse.GetSingleUserResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserApiTest extends BaseApiTest {
 
+  User user = createUser();
+
   @Test
-  @Description("Verify that GET /users?page=2 returns a list of users")
-  void getListOfUsers() {
-    givenWithLogging()
+  @DisplayName("Verify that GET /users?page=2 returns a list of users")
+  void getListOfUsersTest() {
+    GetMultiUserResponse response =
+        givenWithLogging()
             .when()
             .get("/users?page=2")
             .then()
-            .log().status()
-            .log().body()
+            .log()
+            .status()
+            .log()
+            .body()
             .statusCode(200)
-            .body("page", equalTo(2))
-            .body("data", not(empty()));
+            .extract()
+            .as(GetMultiUserResponse.class);
+    assertEquals(2, response.getPage(), ERROR_NOT_AS_EXPECTED.formatted("number"));
+    assertFalse(response.getData().isEmpty(), ERROR_MUST_NOT_BE_EMPTY.formatted("data"));
   }
 
   @Test
-  @Description("Verify that GET /users/2 returns correct user details")
-  void getSingleUser() {
-    givenWithLogging()
+  @DisplayName("Verify that GET /users/2 returns correct user details")
+  void getSingleUserTest() {
+    GetSingleUserResponse response =
+        givenWithLogging()
             .when()
             .get("/users/2")
             .then()
-            .log().status()
-            .log().body()
+            .log()
+            .status()
+            .log()
+            .body()
             .statusCode(200)
-            .body("data.id", equalTo(2))
-            .body("data.email", containsString("@reqres.in"));
+                .extract()
+            .as(GetSingleUserResponse.class);
+    assertEquals(2, response.getData().getId(), ERROR_NOT_AS_EXPECTED.formatted("data.id"));
+    assertTrue(response.getData().getEmail().contains("@reqres.in"), ERROR_NOT_AS_EXPECTED.formatted("email"));
   }
 
   @Test
-  @Description("Verify that POST /users creates a new user")
-  void createUser() {
-    givenWithLogging()
+  @DisplayName("Verify that POST /users creates a new user")
+  void createUserWithValidTokenTest() {
+    UserResponse userResponse =
+        givenWithLogging()
             .contentType(ContentType.JSON)
-            .body("{\"name\": \"Rodion\", \"job\": \"QA Engineer\"}")
+            .body(user)
             .when()
             .post("/users")
             .then()
-            .log().status()
-            .log().body()
+            .log()
+            .status()
+            .log()
+            .body()
             .statusCode(201)
-            .body("name", equalTo("Rodion"))
-            .body("job", equalTo("QA Engineer"))
-            .body("id", notNullValue())
-            .body("createdAt", notNullValue());
+            .extract()
+            .as(UserResponse.class);
+    assertEquals(user.name, userResponse.getName(), ERROR_NOT_AS_EXPECTED.formatted("name"));
+    assertEquals(user.job, userResponse.getJob(), ERROR_NOT_AS_EXPECTED.formatted("job"));
+    assertFalse(userResponse.id.isEmpty(), ERROR_MUST_NOT_BE_EMPTY.formatted("id"));
+    assertFalse(userResponse.createdAt.isEmpty(), ERROR_MUST_NOT_BE_EMPTY.formatted("createdAt"));
   }
 
   @Test
-  @Description("Verify that POST /users fail with 403 on a new user creates with invalid api-key")
-  void createUserWithInvalidApiKey() {
+  @DisplayName("Verify that POST /users fail with 403 on a new user creates with invalid api-key")
+  void createUserWithInvalidApiKeyTest() {
     givenWithLogging()
-            .contentType(ContentType.JSON)
-            .body("{\"name\": \"Rodion\", \"job\": \"QA Engineer\"}")
-            .header("x-api-key","invalid")
-            .when()
-            .post("/users")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(403)
-            .body("error", equalTo("Invalid or inactive API key"));
+        .contentType(ContentType.JSON)
+        .header("x-api-key", "invalid")
+        .body(user)
+        .when()
+        .post("/users")
+        .then()
+        .log()
+        .status()
+        .log()
+        .body()
+        .statusCode(403)
+        .body("error", equalTo(ERROR_INVALID_API_KEY));
+  }
+
+  private User createUser() {
+    return User.builder().name("John").job("AQA Engineer").build();
   }
 }
